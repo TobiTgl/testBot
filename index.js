87dde6e4f6f2de37ac448f64d9c43f72cb306e5e
TobiTgl <tobias_813@t-online.de>
2022-01-02 19:28:22 +0100

    Alle funktionen abgedeckt

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express()
 const port = 3000
 const axios = require('axios');
 var base64 = require('base-64');
-  
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var CronJob = require('cron').CronJob;
 
-var von = "testvon"
+/*
+var dateNow = Date.now()
+    
+var von = (dateNow/1000) + 172800
 
-var bis = "testbis"
+var bis = von + 300
 
 var termin = {"termin": {"bis": von,"von": bis,"maximal": 4,"bereitsgebucht": 0,"buchbareZeitID": 5557,"gruppenID": 137,"guid": "","nochfrei": 0,"resourceID": 235,"terminArtID": 155},"terminPatient": {"ausweisnummer": null,"befundTransmitMethods": ["email","sms"],"eMail": "tobias_813@t-online.de","geburtstag": 859676400,"geschlecht": "M","handyNr": "015172662243","hausnr": "10","id": 0,"name": "Tgel","ort": "Illerrieden","plz": "89186","strasse": "Bischof-Ketteler Str","vorname": "Tobias"}}
 
@@ -19,29 +19,28 @@ var myString = JSON.stringify(termin)
 var encodedData = base64.encode(myString);
 
 console.log(encodedData)
-
-app.post('/covTest', (req, res) => {
-    res.send('Hello World!')
-  })
-
-var CronJob = require('cron').CronJob;
+*/
+app.get('/', (req, res) => {
+  res.send("HelloWorld!")
+})
 
 var job = new CronJob('* * * * *', function(){
 
-    var data = 'eyJ0ZXJtaW4iOiB7ImJpcyI6IDE2MzkzOTU5MDAsInZvbiI6IDE2MzkzOTU2MDAsIm1heGltYWwiOiA0LCJiZXJlaXRzZ2VidWNodCI6IDAsImJ1Y2hiYXJlWmVpdElEIjogNTU1NywiZ3J1cHBlbklEIjogMTM3LCJndWlkIjogIiIsIm5vY2hmcmVpIjogMCwicmVzb3VyY2VJRCI6IDIzNSwidGVybWluQXJ0SUQiOiAxNTV9LCJ0ZXJtaW5QYXRpZW50IjogeyJhdXN3ZWlzbnVtbWVyIjogbnVsbCwiYmVmdW5kVHJhbnNtaXRNZXRob2RzIjogWyJlbWFpbCIsInNtcyJdLCJlTWFpbCI6ICJ0b2JpYXNfODEzQHQtb25saW5lLmRlIiwiZ2VidXJ0c3RhZyI6IDg1OTY3NjQwMCwiZ2VzY2hsZWNodCI6ICJNIiwiaGFuZHlOciI6ICIwMTUxNzI2NjIyNDMiLCJoYXVzbnIiOiAiMTAiLCJpZCI6IDAsIm5hbWUiOiAiVGdlbCIsIm9ydCI6ICJJbGxlcnJpZWRlbiIsInBseiI6ICI4OTE4NiIsInN0cmFzc2UiOiAiQmlzY2hvZi1LZXR0ZWxlciBTdHIiLCJ2b3JuYW1lIjogIlRvYmlhcyJ9fQ';
+  var data = 'eyJ0ZXJtaW4iOiB7ImJpcyI6IDE2MzkzOTU5MDAsInZvbiI6IDE2MzkzOTU2MDAsIm1heGltYWwiOiA0LCJiZXJlaXRzZ2VidWNodCI6IDAsImJ1Y2hiYXJlWmVpdElEIjogNTU1NywiZ3J1cHBlbklEIjogMTM3LCJndWlkIjogIiIsIm5vY2hmcmVpIjogMCwicmVzb3VyY2VJRCI6IDIzNSwidGVybWluQXJ0SUQiOiAxNTV9LCJ0ZXJtaW5QYXRpZW50IjogeyJhdXN3ZWlzbnVtbWVyIjogbnVsbCwiYmVmdW5kVHJhbnNtaXRNZXRob2RzIjogWyJlbWFpbCIsInNtcyJdLCJlTWFpbCI6ICJ0b2JpYXNfODEzQHQtb25saW5lLmRlIiwiZ2VidXJ0c3RhZyI6IDg1OTY3NjQwMCwiZ2VzY2hsZWNodCI6ICJNIiwiaGFuZHlOciI6ICIwMTUxNzI2NjIyNDMiLCJoYXVzbnIiOiAiMTAiLCJpZCI6IDAsIm5hbWUiOiAiVGdlbCIsIm9ydCI6ICJJbGxlcnJpZWRlbiIsInBseiI6ICI4OTE4NiIsInN0cmFzc2UiOiAiQmlzY2hvZi1LZXR0ZWxlciBTdHIiLCJ2b3JuYW1lIjogIlRvYmlhcyJ9fQ';
 
-    var von = "testvon"
+  var dateNow = Math.round(Date.now()/1000)
 
-    var bis = "testbis"
+  console.log(dateNow)
+    
+  var von = dateNow + 172800
 
-    var termin = {"termin": {"bis": von,"von": bis,"maximal": 4,"bereitsgebucht": 0,"buchbareZeitID": 5557,"gruppenID": 137,"guid": "","nochfrei": 0,"resourceID": 235,"terminArtID": 155},"terminPatient": {"ausweisnummer": null,"befundTransmitMethods": ["email","sms"],"eMail": "tobias_813@t-online.de","geburtstag": 859676400,"geschlecht": "M","handyNr": "015172662243","hausnr": "10","id": 0,"name": "Tgel","ort": "Illerrieden","plz": "89186","strasse": "Bischof-Ketteler Str","vorname": "Tobias"}}
+  var bis = von + 300
 
-console.log(termin.termin)
+  var termin = {"termin": {"bis": von,"von": bis,"maximal": 4,"bereitsgebucht": 0,"buchbareZeitID": 5557,"gruppenID": 137,"guid": "","nochfrei": 0,"resourceID": 235,"terminArtID": 155},"terminPatient": {"ausweisnummer": null,"befundTransmitMethods": ["email","sms"],"eMail": "tobias_813@t-online.de","geburtstag": 859676400,"geschlecht": "M","handyNr": "015172662243","hausnr": "10","id": 0,"name": "Tgel","ort": "Illerrieden","plz": "89186","strasse": "Bischof-Ketteler Str","vorname": "Tobias"}}
 
-    var encodedData = base64.encode(termin);
+  var myString = JSON.stringify(termin)
 
-    console.log(JSON.stringify(encodedData))
-
+  var encodedData = base64.encode(myString);
 
     var config = {
         method: 'post',
@@ -49,7 +48,7 @@ console.log(termin.termin)
         headers: { 
         'Content-Type': 'text/plain'
         },
-        data : data
+        data : encodedData
     };
 
     axios(config)
@@ -60,7 +59,7 @@ console.log(termin.termin)
         console.log(error);
     });
 
-} , null, true, 'America/Los_Angeles');
+} , null, true, 'Europe/Berlin');
 job.start();
 
 app.listen(port, () => {
