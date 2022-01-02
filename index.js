2252407c8bb5019feb2cf3839930bd0ebd9cfc92
TobiTgl <tobias_813@t-online.de>
2022-01-02 20:10:00 +0100

    pre back to munic test

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   res.send("HelloWorld!")
 })
 
-var job = new CronJob('05 20 * * SUN', function(){
+var job = new CronJob('40 12 * * 1,3,6', function(){
 
   var data = 'eyJ0ZXJtaW4iOiB7ImJpcyI6IDE2MzkzOTU5MDAsInZvbiI6IDE2MzkzOTU2MDAsIm1heGltYWwiOiA0LCJiZXJlaXRzZ2VidWNodCI6IDAsImJ1Y2hiYXJlWmVpdElEIjogNTU1NywiZ3J1cHBlbklEIjogMTM3LCJndWlkIjogIiIsIm5vY2hmcmVpIjogMCwicmVzb3VyY2VJRCI6IDIzNSwidGVybWluQXJ0SUQiOiAxNTV9LCJ0ZXJtaW5QYXRpZW50IjogeyJhdXN3ZWlzbnVtbWVyIjogbnVsbCwiYmVmdW5kVHJhbnNtaXRNZXRob2RzIjogWyJlbWFpbCIsInNtcyJdLCJlTWFpbCI6ICJ0b2JpYXNfODEzQHQtb25saW5lLmRlIiwiZ2VidXJ0c3RhZyI6IDg1OTY3NjQwMCwiZ2VzY2hsZWNodCI6ICJNIiwiaGFuZHlOciI6ICIwMTUxNzI2NjIyNDMiLCJoYXVzbnIiOiAiMTAiLCJpZCI6IDAsIm5hbWUiOiAiVGdlbCIsIm9ydCI6ICJJbGxlcnJpZWRlbiIsInBseiI6ICI4OTE4NiIsInN0cmFzc2UiOiAiQmlzY2hvZi1LZXR0ZWxlciBTdHIiLCJ2b3JuYW1lIjogIlRvYmlhcyJ9fQ';
 
@@ -36,7 +36,7 @@ var job = new CronJob('05 20 * * SUN', function(){
 
   var bis = von + 300
 
-  var termin = {"termin": {"bis": von,"von": bis,"maximal": 4,"bereitsgebucht": 0,"buchbareZeitID": 5557,"gruppenID": 137,"guid": "","nochfrei": 0,"resourceID": 235,"terminArtID": 155},"terminPatient": {"ausweisnummer": null,"befundTransmitMethods": ["email","sms"],"eMail": "tobias_813@t-online.de","geburtstag": 859676400,"geschlecht": "M","handyNr": "015172662243","hausnr": "10","id": 0,"name": "Tgel","ort": "Illerrieden","plz": "89186","strasse": "Bischof-Ketteler Str","vorname": "Tobias"}}
+  var termin = {"termin": {"bis": bis,"von": von,"maximal": 4,"bereitsgebucht": 0,"buchbareZeitID": 5557,"gruppenID": 137,"guid": "","nochfrei": 0,"resourceID": 235,"terminArtID": 155},"terminPatient": {"ausweisnummer": null,"befundTransmitMethods": ["email","sms"],"eMail": "tobias_813@t-online.de","geburtstag": 859676400,"geschlecht": "M","handyNr": "015172662243","hausnr": "10","id": 0,"name": "Tgel","ort": "Illerrieden","plz": "89186","strasse": "Bischof-Ketteler Str","vorname": "Tobias"}}
 
   var myString = JSON.stringify(termin)
 
